26a6a95a3a919e97464881129a2f3082e1984740
dingding231 <kacmlweimer@gmail.com>
2013-06-12 12:47:58 -0500

    weird glitch happens when i adjust the jump values

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ addEventListener("keyup", function (e) {
 }, false);
 
 function jump(player) {
-	var jumpTime = 150,
+	var jumpTime = 250,
 		height = 15,
 		jumpSpeed = 40,
 		i;
@@ -97,7 +97,7 @@ function jump(player) {
 				clearInterval(i);
 			}, jumpTime);
 			i = setInterval( function() {
-				player.y += player.speed  / height;
+				player.y += player.speed / height;
 			}, 40);
 		}, jumpTime);
 	}
